b0d5225b70c24645ab35401feb4cbba33d725b79
mhu <marcel.huismann@protonmail.ch>
2022-07-23 20:01:47 -0400

    feat: created first version

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+import openai
+from discord import Message
+from discord.ext import commands
+from discord.ext.commands import Context
+
+ENGINE = "text-davinci-002"
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!openai', intents=intents)
+
+
+@bot.command()
+async def on_message(context: Context):
+    message: Message = context.message
+
+    completion = openai.Completion.create(engine=ENGINE, prompt=message.content)
+
+    output = completion.choices[0].text.strip()
+
+    await context.send(output)
+
+
+bot.run('token')
